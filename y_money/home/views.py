250298b1fe0f827a77082994f03f8d3cab61c07c
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-03-12 20:54:20 +0100

    Refactor project structure: remove unused apps, keep config and users

--- y_money/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def homeDashboard(request):
-    return render(request, 'home/homePage.html')
--- y_money/myFinances/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyfinancesConfig(AppConfig):
-    name = 'myFinances'
