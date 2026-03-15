618c19edced3554421893ae4eeb8bf5f46b8fc7d
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-03-15 10:36:33 +0100

    add login/logout functionality

--- y_money/apps/users/views.py
+++ y_money/apps/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+class UserLoginView(LoginView):
+    pass
+
+    
+class UserLogoutView(LogoutView):
+    pass
--- y_money/config/settings.py
+++ y_money/config/settings.py
@@ -151,3 +151,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login config
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
