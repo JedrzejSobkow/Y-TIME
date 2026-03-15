0404ee058d96f17cd0f7adeec05ac39d6a884282
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-03-15 14:29:47 +0100

    Add user register form

--- y_money/apps/users/views.py
+++ y_money/apps/users/views.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from .forms import RegisterForm
+from django.urls import reverse_lazy
 
 class UserLoginView(LoginView):
-    pass
+    template_name = "registration/login.html"
 
     
 class UserLogoutView(LogoutView):
     pass
+
+
+class UserRegisterView(CreateView):
+    
+    form_class = RegisterForm
+    
+    template_name = "registration/register.html"
+    
+    success_url = reverse_lazy("login")
