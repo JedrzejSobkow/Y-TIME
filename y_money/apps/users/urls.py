d03b6733fa763bde8e7ffe8328c9cd4092ca177a
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-03-16 21:23:36 +0100

    Add views for friends list, pending requests and sent requests

--- y_money/apps/users/urls.py
+++ y_money/apps/users/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from .views import UserLoginView, UserLogoutView, UserRegisterView
+from .views import FriendsPanelView, FriendsListView, PendingRequestsListView, SentRequestsListView
 
 urlpatterns = [
+    # TODO move it to the core urls
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
+    
+    path('friends/', FriendsPanelView.as_view(), name='frineds_panel'),
+    path('friends/friends_list', FriendsListView.as_view(), name='friends_list'),
+    path('friends/pending_list', PendingRequestsListView.as_view(), name='pending_list'),
+    path('friends/sent_list', SentRequestsListView.as_view(), name='sent_list'),
+    # path('friends/add/<int:profile_id>/', SendFriendRequestView.as_view(), name='send-friend-request'),
+    # path('friends/request/accept/<int:request_id>/', AcceptFriendRequestView.as_view(), name='accept-friend-request'),
+    # path('friends/request/reject/<int:request_id>/', RejectFriendRequestView.as_view(), name='reject-friend-request'),
+    # path('friends/request/cancel/<int:request_id>/', CancelFriendRequestView.as_view(), name='cancel-friend-request'),
 ]
--- y_money/apps/users/views.py
+++ y_money/apps/users/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView
-from .forms import RegisterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, TemplateView
 from django.urls import reverse_lazy
 
+from .forms import RegisterForm
+from .models import Profile
+
+
+# TODO move it to the core views
 class UserLoginView(LoginView):
     template_name = "registration/login.html"
 
@@ -12,9 +17,42 @@ class UserLogoutView(LogoutView):
 
 
 class UserRegisterView(CreateView):
-    
     form_class = RegisterForm
-    
     template_name = "registration/register.html"
-    
     success_url = reverse_lazy("login")
+    
+    
+
+    
+class FriendsPanelView(LoginRequiredMixin, TemplateView):
+    template_name = "friends/panel.html"
+    
+class FriendsListView(LoginRequiredMixin, ListView):
+    template_name = "friends/partials/friends_list.html"
+    context_object_name = "friends"
+    
+    def get_queryset(self):
+        profile = Profile.objects.select_related('user').get(user=self.request.user)
+        return profile.get_friends()
+    
+class PendingRequestsListView(LoginRequiredMixin, ListView):
+    template_name = "friends/partials/pending_list.html"
+    context_object_name = "pending_requests"
+    
+    def get_queryset(self):
+        return self.request.user.profile.get_pending_friend_requests()
+    
+    
+class SentRequestsListView(LoginRequiredMixin, ListView):
+    template_name = "friends/partials/sent_list.html"
+    context_object_name = "sent_requests"
+    
+    def get_queryset(self):
+        return self.request.user.profile.get_sent_friend_requests()
+    
+
+    
+    
+    
+    
+    
