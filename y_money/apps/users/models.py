caebbe900b40aefdba9b180641217df88d1f2b46
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-03-16 21:22:45 +0100

    Update users models. added some helper functions to automaticaly create profile when user created

--- y_money/apps/users/models.py
+++ y_money/apps/users/models.py
@@ -1,18 +1,52 @@
 from django.db import models
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from apps.core.models import TimeStampedModel
 from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.db.models import Q
+from django.db.models.signals import post_save
 
 
 class Profile(TimeStampedModel):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-        
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name='profile')
         
         
     def __str__(self):
         return f"[{self.user.username}] {self.user.first_name} {self.user.last_name}"
     
+    def get_friends(self):
+        return Profile.objects.select_related(
+            "user"
+        ).filter(
+            Q(friendships_initiated__profile2 = self) |
+            Q(friendships_received__profile1 = self)
+        )
     
+    def get_pending_friend_requests(self):
+        return Profile.objects.select_related(
+            "user"
+        ).filter(
+            sent_friend_requests__to_profile = self,
+            sent_friend_requests__status = FriendRequest.Status.PENDING
+        )
+    
+    def get_sent_friend_requests(self):
+        return Profile.objects.select_related(
+            "user"
+        ).filter(
+            received_friend_requests__from_profile = self,
+            received_friend_requests__status = FriendRequest.Status.PENDING
+        )
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
     
     
 class FriendRequest(TimeStampedModel):
@@ -22,22 +56,20 @@ class FriendRequest(TimeStampedModel):
         REJECTED = "rejected", "Rejected"
         CANCELLED = "cancelled", "Cancelled"
         
-        
 
-    from_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="sent_friend_requests")
-    to_profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="received_friend_requests")
-    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
-
+    from_profile = models.ForeignKey(Profile, on_delete = models.CASCADE, related_name = "sent_friend_requests")
+    to_profile = models.ForeignKey(Profile, on_delete = models.CASCADE, related_name = "received_friend_requests")
+    status = models.CharField(max_length = 20, choices = Status.choices, default = Status.PENDING)
 
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["from_profile", "to_profile"], name="unique_friend_request")
+            models.UniqueConstraint(fields = ["from_profile", "to_profile"], name = "unique_friend_request")
         ]
         
         indexes = [
-            models.Index(fields=["from_profile", "status"]),
-            models.Index(fields=["to_profile", "status"])
+            models.Index(fields = ["from_profile", "status"]),
+            models.Index(fields = ["to_profile", "status"])
         ]
 
     def __str__(self):
@@ -47,31 +79,68 @@ class FriendRequest(TimeStampedModel):
         if self.from_profile == self.to_profile:
             raise ValidationError("Cannot send friend request to yourself")
         
+        if FriendRequest.objects.filter(
+            (
+                (Q(from_profile = self.to_profile) & Q(to_profile = self.from_profile)) |
+                (Q(from_profile = self.from_profile) & Q(to_profile = self.to_profile))
+            ) & Q(status = FriendRequest.Status.PENDING)
+        ).exists():
+            raise ValidationError("There is already one friend request pending in this relation")
+        
+        if Friendship.objects.filter(
+            (Q(profile1 = self.to_profile) & Q(profile2 = self.from_profile)) |
+            (Q(profile1 = self.from_profile) & Q(profile2 = self.to_profile))
+        ).exists():
+            raise ValidationError("You are already friends with this user")
         
         
-    def accept(self):
-        self.status = self.Status.ACCEPTED
+    def accept(self, profile):
+        if self.status != self.Status.PENDING:
+            raise ValidationError("Request is not pending")
+        
+        if self.to_profile != profile:
+            raise ValidationError("You cannot accept this request")
+        
+        with transaction.atomic():
+            self.status = self.Status.ACCEPTED
+            self.save()
+            Friendship.objects.create(profile1 = self.from_profile, profile2 = self.to_profile)
+        
+    def reject(self, profile):
+        if self.status != self.Status.PENDING:
+            raise ValidationError("Request is not pending")
+        
+        if self.to_profile != profile:
+            raise ValidationError("You cannot reject this request")
+        
+        self.status = self.Status.REJECTED
         self.save()
         
-        Friendship.objects.create(from_profile = self.from_profile, to_profile = self.to_profile)
-    
+    def cancel(self, profile):
+        if self.status != self.Status.PENDING:
+            raise ValidationError("Request is not pending")
+        
+        if self.from_profile != profile:
+            raise ValidationError("You cannot cancel this request")
+        
+        self.status = self.Status.CANCELLED
+        self.save()
     
     
     
 class Friendship(TimeStampedModel):
-    profile1 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="friendships_initiated")
-    profile2 = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="friendships_received")
-    
+    profile1 = models.ForeignKey(Profile, on_delete = models.CASCADE, related_name = "friendships_initiated")
+    profile2 = models.ForeignKey(Profile, on_delete = models.CASCADE, related_name = "friendships_received")
     
     
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["profile1", "profile2"], name="unique_friendship")
+            models.UniqueConstraint(fields = ["profile1", "profile2"], name = "unique_friendship")
         ]
         
         indexes = [
-            models.Index(fields=["profile1"]),
-            models.Index(fields=["profile2"])
+            models.Index(fields = ["profile1"]),
+            models.Index(fields = ["profile2"])
         ]
         
     def __str__(self):
