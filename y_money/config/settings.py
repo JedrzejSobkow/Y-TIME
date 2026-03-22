329d9f86216387d9586395aecc4ae163f4d17728
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-03-22 11:03:07 +0100

    Add error when no django secret provided

--- y_money/config/settings.py
+++ y_money/config/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+if not SECRET_KEY:    
+    raise ValueError("DJANGO_SECRET_KEY environment variable is required!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
