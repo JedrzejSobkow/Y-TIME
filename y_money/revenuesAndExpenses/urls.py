442e85579c5c85ee0ebae8a5b1db881b285ac1da
Jędrzej Sobków <sobkowjedrzej@gmail.com>
2026-04-06 07:20:12 +0200

    remove unused app

--- y_money/revenuesAndExpenses/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', view=views.addRevenueOrExpenseView, name='add-revenue-or-expense')
-]
--- y_money/revenuesAndExpenses/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def addRevenueOrExpenseView(request):
-    return render(request, 'revenuesAndExpenses/AddRevenueOrExpensePage.html')
